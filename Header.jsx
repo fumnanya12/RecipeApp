68827e48380a4a6a2cb1d0a646258825fc4f906a
Fumnanya obi <fumnanya.obi@gmail.com>
2025-01-03 12:03:41 -0600

    rearranging files

--- Header.jsx
+++
@@ -1,8 +0,0 @@
-export default function Header() {
-    return (
-        <header className="App-header">
-            <img src="/images/chef-claude-icon.png" className="App-logo" alt="logo" />
-            <h1>Chef Claude</h1>
-        </header>
-    )
-}
